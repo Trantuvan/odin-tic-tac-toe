906fab0f54376515bfbe8a2902726fd15ebca68e
trantuvan <trantuvan.kan@gmail.com>
2022-07-25 12:16:38 +0700

    Implement cpu play

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,3 @@
-
-// player factory: Properties => name, marker
-// to define inside Player factory: getname, getMarker, setNamen, setMarker (return these 4)
 const player = (marker) =>{
   const getMarker = () => marker;
   const setMarker = (value) => marker = value;
@@ -8,13 +5,6 @@ const player = (marker) =>{
   return {getMarker, setMarker};
 }
 
-// another factory => store data inside of it
-// create the board. 9 fields of the board, what value?
-// create a function to find an element at a specific index of the board
-// create function to set a value to a specific index of the board
-// create a function that will get you a state of the board at any given time (you want to return a copy of the board because of data privacy.
-// This way, your original board stays inside of your factory and in that way it's protected. You only ever work with the copy)
-// so inside of this one you return : getValueOfTheBoardAtIndex, setValueOfTheBoardAtIndex, getBoard (you probably want to name this better :D)
 const board = () =>{
   let board = [
     undefined, undefined, undefined,
@@ -33,15 +23,11 @@ const board = () =>{
   return { getBoard, setBoardValueIndex, setBoardDefault };
 }
 
-// and finally, an IIFE. This is where you define your game logic. Here you define players, define a function that will define which player is
-// the current player, the clear function, checkWinner function, and you attach event listeners to divs. Once you defined all of the above, this part should
-// not be too complicated. In this event listener, what you want is to actually tie your state of the board with the DOM. Dom should be used only to display
-// our data, it should not be our primary source of data. There you go. I go back you can ask questions in discord
-
 (() => {
   const gameBoard = board();
   const player1 = player("X");
   const player2 = player("O");
+  let cpuPlayer;
   let player1Turns = true;
 
   //* cache dom
@@ -50,6 +36,7 @@ const board = () =>{
   const replayBtn = wrapper.querySelector(".action > .replay-btn");
   const currentMarker = wrapper.querySelector("#current-marker");
   const cells = wrapper.querySelectorAll("#game-board > .cell");
+  const cpuBtn = wrapper.querySelector(".right-opponent");
   const modal = document.querySelector(".modal");
   const modalInfo = modal.querySelector(".modal-info");
   const modalReplayBtn = modal.querySelector(".modal-action > .btn");
@@ -59,14 +46,24 @@ const board = () =>{
     const player2Marker = player1Marker === "X" ? "O" : "X";
     player1.setMarker(player1Marker);
     player2.setMarker(player2Marker);
+
+    if (cpuPlayer !== undefined) {
+      cpuPlayer.setMarker(player2Marker);
+    }
   }
 
-  function getCurrentPlayer() {
+  function  getCurrentPlayer() {
     if (player1Turns) {
       player1Turns = false;
       return player1;
     }
+
     player1Turns = true;
+
+    if(cpuPlayer !== undefined) {
+      return cpuPlayer;
+    }
+
     return player2;
   }
 
@@ -108,10 +105,53 @@ const board = () =>{
     }
   }
 
+  function renderCellMarkerByCpu(currentPlayer, status, cellIndex) {
+    let playerMarker = currentPlayer.getMarker();
+
+    if (playerMarker === "X") {
+        Array.from(cells)[
+          `${cellIndex}`
+        ].innerHTML = `<img src="./assets/img/x-marker.png" alt="x-marker">`;
+    }
+
+    if (playerMarker === "O") {
+      Array.from(cells)[`${cellIndex}`].innerHTML =
+      `<img src="./assets/img/o-marker.png" alt="o-marker">`;
+    }
+
+    if (status.winStatus === true) {
+      const winCells = status.winCase;
+      const cellsArray = Array.from(cells);
+
+      switch (playerMarker) {
+        case "X":
+          winCells.forEach(
+            (cellIndex) =>
+              (cellsArray[cellIndex].style.backgroundColor = "#22c55e")
+          );
+          break;
+
+        case "O":
+          winCells.forEach(
+            (cellIndex) =>
+              (cellsArray[cellIndex].style.backgroundColor = "#f43f5e")
+          );
+          break;
+      }
+    }
+  }
+
   function updateBoard(evt,currentPlayer) {
     const cellIndex = evt.target.getAttribute("data-index");
     const playermarker = currentPlayer.getMarker();
     gameBoard.setBoardValueIndex(cellIndex, playermarker);
+  }
+
+  function updateBoardByCpu(currentPlayer) {
+    const cellIndex = gameBoard.getBoard().findIndex(cellValue => cellValue === undefined);
+    const playermarker = currentPlayer.getMarker();
+    gameBoard.setBoardValueIndex(cellIndex, playermarker);
+    return cellIndex;
   }
 
   function checkWinner(currentPlayer) {
@@ -176,7 +216,15 @@ const board = () =>{
     if(modal.classList.contains("deactivated") === false) {
       modal.classList.add("deactivated");
     }
+
+    player1.setMarker("X");
+    player2.setMarker("O");
+    player1Turns = true;
+    currentMarker.textContent = "X";
   }
+
+  //* cpu Player
+  cpuBtn.addEventListener("click", () => cpuPlayer = player("O"));
 
   //* change marker
   Array.from(markers).forEach(marker => {
@@ -204,6 +252,18 @@ const board = () =>{
       renderMarkerStatus(currentPlayer);
       renderCellMarker(evt,currentPlayer, status);
       renderModal(status);
+
+      if(cpuPlayer !== undefined) {
+        currentPlayer = getCurrentPlayer();
+        const cpuIndex = updateBoardByCpu(currentPlayer);
+        const status = checkWinner(currentPlayer);
+        setTimeout(()=>{
+          renderMarkerStatus.call(this,currentPlayer)
+          renderCellMarkerByCpu.call(this, currentPlayer, status, cpuIndex)
+        },
+          1000);
+        renderModal(status);
+      }
     })
-  })
+  });
 })()
